40af2fb6863f4f6a81efc7b5fcfe15b8f9060cf2
Sebastian Stern <sebastian.stern.42@gmail.com>
2021-10-27 09:51:00 +0200

    Adds roundDown

--- Math.lua
+++ Math.lua
@@ -1,16 +1,38 @@
 local Math = {}
 
+--- Same as Math.roundUp()
 ---@overload fun(value: number): number
 ---@param value number
----@param decimalPlaces number
+---@param decimalPlaces number @Defaults to 0.
 ---@return number
 function Math.round(value, decimalPlaces)
+    return Math.roundUp(value, decimalPlaces)
+end
+
+---@overload fun(value: number): number
+---@param value number
+---@param decimalPlaces number @Defaults to 0.
+---@return number
+function Math.roundUp(value, decimalPlaces)
     if decimalPlaces and decimalPlaces > 0 then
         local multiple = 10 ^ decimalPlaces
         return math.floor(value * multiple + 0.5) / multiple
     end
 
     return math.floor(value + 0.5)
+end
+
+---@overload fun(value: number): number
+---@param value number
+---@param decimalPlaces number @Defaults to 0.
+---@return number
+function Math.roundDown(value, decimalPlaces)
+    if decimalPlaces and decimalPlaces > 0 then
+        local multiple = 10 ^ decimalPlaces
+        return math.ceil(value * multiple + 0.5) / multiple
+    end
+
+    return math.ceil(value - 0.5)
 end
 
 return Math
