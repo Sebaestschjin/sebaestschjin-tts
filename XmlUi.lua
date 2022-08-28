58eea6ed7d6adb04122c2e576aafe0dc44c3d3da
Sebastian Stern <sebastian.stern.42@gmail.com>
2022-08-28 15:31:32 +0200

    feat: Add function to remove an asset

--- XmlUi.lua
+++ XmlUi.lua
@@ -32,199 +32,218 @@ XmlUi.Factory = XmlUiFactory
 
 --- Values available for alignment attributes.
 XmlUi.Alignment = {
-    UpperLeft = "UpperLeft",
-    UpperCenter = "UpperCenter",
-    UpperRight = "UpperRight",
-    MiddleLeft = "MiddleLeft",
-    MiddleCenter = "MiddleCenter",
-    MiddleRight = "MiddleRight",
-    LowerLeft = "LowerLeft",
-    LowerCenter = "LowerCenter",
-    LowerRight = "LowerRight",
+  UpperLeft = "UpperLeft",
+  UpperCenter = "UpperCenter",
+  UpperRight = "UpperRight",
+  MiddleLeft = "MiddleLeft",
+  MiddleCenter = "MiddleCenter",
+  MiddleRight = "MiddleRight",
+  LowerLeft = "LowerLeft",
+  LowerCenter = "LowerCenter",
+  LowerRight = "LowerRight",
 }
 
 --- Values available for animation attributes.
 XmlUi.Animation = {
-    Show = {
-        None = "None",
-        Grow = "Grow",
-        FadeIn = "FadeIn",
-        SlideInLeft = "SlideIn_Left",
-        SlideInRight = "SlideIn_Right",
-        SlideInTop = "SlideIn_Top",
-        SlideInBottom = "SlideIn_Bottom",
-    },
-    Hide = {
-        None = "None",
-        Shrink = "Shrink",
-        FadeOut = "FadeOut",
-        SlideOut_Left = "SlideOut_Left",
-        SlideOutRight = "SlideOutRight",
-        SlideOutTop = "SlideOut_Top",
-        SlideOutBottom = "SlideOutBottom",
-    },
+  Show = {
+    None = "None",
+    Grow = "Grow",
+    FadeIn = "FadeIn",
+    SlideInLeft = "SlideIn_Left",
+    SlideInRight = "SlideIn_Right",
+    SlideInTop = "SlideIn_Top",
+    SlideInBottom = "SlideIn_Bottom",
+  },
+  Hide = {
+    None = "None",
+    Shrink = "Shrink",
+    FadeOut = "FadeOut",
+    SlideOut_Left = "SlideOut_Left",
+    SlideOutRight = "SlideOutRight",
+    SlideOutTop = "SlideOut_Top",
+    SlideOutBottom = "SlideOutBottom",
+  },
 }
 
 --- Values available for fontStyle attributes.
 XmlUi.FontStyle = {
-    Bold = "Bold",
-    BoldAndItalic = "BoldAndItalic",
-    Italic = "Italic",
-    Normal = "Normal",
+  Bold = "Bold",
+  BoldAndItalic = "BoldAndItalic",
+  Italic = "Italic",
+  Normal = "Normal",
 }
 
 XmlUi.GridLayout = {
-    FixedColumnCount = "FixedColumnCount"
+  FixedColumnCount = "FixedColumnCount"
 }
 
 XmlUi.MouseEvent = {
-    LeftClick = "-1",
-    RightClick = "-2",
-    MiddleClick = "-3",
-    SingleTouch = "1",
-    DoubleTouch = "2",
-    TripleTouch = "3",
+  LeftClick = "-1",
+  RightClick = "-2",
+  MiddleClick = "-3",
+  SingleTouch = "1",
+  DoubleTouch = "2",
+  TripleTouch = "3",
 }
 
-setmetatable(XmlUi, TableUtil.merge(getmetatable(XmlUiContainer), {
-    ---@param object tts__Object
-    __call = function(_, object)
-        local self = --[[---@type seb_XmlUi]] XmlUiContainer()
-        local boundObject = object
-        local children = self._wrapChildren(boundObject.UI.getXmlTable())
-        ---@type tts__UIAsset[]
-        local assets = boundObject.UI.getCustomAssets()
-        local assetsChanged = false
+---@param object tts__Object
+local function new(object)
+  local self = --[[---@type seb_XmlUi]] XmlUiContainer()
+  local boundObject = object
+  local children = self._wrapChildren(boundObject.UI.getXmlTable())
 
-        ---@return tts__UIElement[]
-        local function createXmlTable()
-            return TableUtil.map(children, function(child) return child.getXmlElement() end)
-        end
+  ---@type tts__UIAsset[]
+  local assets = boundObject.UI.getCustomAssets()
+  local assetsChanged = false
 
-        ---@param childElements seb_XmlUi_Element[]
-        ---@param elementId string
-        local function findElementById(childElements, elementId)
-            for _, element in pairs(childElements) do
-                if element.getId() == elementId then
-                    return element
-                end
-                local inChild = findElementById(element.getChildren(), elementId)
-                if inChild then
-                    return inChild
-                end
-            end
-        end
+  ---@return tts__UIElement[]
+  local function createXmlTable()
+    return TableUtil.map(children, function(child) return child.getXmlElement() end)
+  end
 
-        ---@param elementId tts__UIElement_Id
-        ---@return nil | seb_XmlUi_Element
-        function self.findElement(elementId)
-            return findElementById(children, elementId)
-        end
+  ---@param childElements seb_XmlUi_Element[]
+  ---@param elementId string
+  local function findElementById(childElements, elementId)
+    for _, element in pairs(childElements) do
+      if element.getId() == elementId then
+        return element
+      end
+      local inChild = findElementById(element.getChildren(), elementId)
+      if inChild then
+        return inChild
+      end
+    end
+  end
 
-        ---@param elementId string
+  ---@param elementId tts__UIElement_Id
+  ---@return nil | seb_XmlUi_Element
+  function self.findElement(elementId)
+    return findElementById(children, elementId)
+  end
+
+  ---@param elementId string
         ---@param attribute string
         ---@param value string | number | boolean
-        function self.setAttribute(elementId, attribute, value)
-            boundObject.UI.setAttribute(elementId, attribute, value)
-        end
+  function self.setAttribute(elementId, attribute, value)
+    boundObject.UI.setAttribute(elementId, attribute, value)
+  end
 
-        ---@param element seb_XmlUi_Element
-        function self.addChild(element)
-            element.bindUi(self) -- TODO propagate to children
+  ---@param element seb_XmlUi_Element
+  function self.addChild(element)
+    element.bindUi(self) -- TODO propagate to children
 
-            for i, child in ipairs(children) do
-                if element.getZIndex() < child.getZIndex() then
-                    table.insert(children, i, element)
-                    return
-                end
-            end
-
-            table.insert(children, element)
-        end
-
-        ---@param elementId tts__UIElement_Id
-        function self.show(elementId)
-            boundObject.UI.show(elementId)
-        end
-
-        ---@param elementId tts__UIElement_Id
-        function self.hide(elementId)
-            boundObject.UI.hide(elementId)
-        end
-
-        function self.update()
-            local xmlTable = createXmlTable()
-            boundObject.UI.setXmlTable(xmlTable)
-        end
-
-        ---@return boolean
-        function self.updateUiAssets()
-            if assetsChanged then
-                boundObject.UI.setCustomAssets(assets)
-                assetsChanged = false
-                return true
-            end
-
-            return false
-        end
-
-        ---@param assetList tts__UIAsset[]
-        function self.updateAssets(assetList)
-            for _, asset in ipairs(assetList) do
-                self.updateAsset(asset.name, asset.url)
-            end
-        end
-
-        ---@param assetName string
-        ---@param assetUrl URL
-        function self.updateAsset(assetName, assetUrl)
-            for _, asset in ipairs(assets) do
-                if asset.name == assetName then
-                    if asset.url ~= assetUrl then
-                        asset.url = assetUrl
-                        assetsChanged = true
-                    end
-                    return
-                end
-            end
-
-            table.insert(assets, { name = assetName, url = assetUrl, })
-            assetsChanged = true
-        end
-
-        ---@return tts__UIAsset[]
-        function self.getAssets()
-            return assets
-        end
-
-        ---@param assetName string
-        ---@return boolean
-        function self.hasAsset(assetName)
-            for _, asset in ipairs(assets) do
-                if asset.name == assetName then
-                    return true
-                end
-            end
-            return false
-        end
-
-        return self
+    for i, child in ipairs(children) do
+      if element.getZIndex() < child.getZIndex() then
+        table.insert(children, i, element)
+        return
+      end
     end
+
+    table.insert(children, element)
+  end
+
+  ---@param elementId tts__UIElement_Id
+  function self.show(elementId)
+    boundObject.UI.show(elementId)
+  end
+
+  ---@param elementId tts__UIElement_Id
+  function self.hide(elementId)
+    boundObject.UI.hide(elementId)
+  end
+
+  function self.update()
+    local xmlTable = createXmlTable()
+    boundObject.UI.setXmlTable(xmlTable)
+  end
+
+  ---@return boolean
+  function self.updateUiAssets()
+    if assetsChanged then
+      boundObject.UI.setCustomAssets(assets)
+      assetsChanged = false
+      return true
+    end
+
+    return false
+  end
+
+  ---@param assetList tts__UIAsset[]
+  function self.updateAssets(assetList)
+    for _, asset in ipairs(assetList) do
+      self.updateAsset(asset.name, asset.url)
+    end
+  end
+
+  ---@param assetName string
+  ---@param assetUrl URL
+  function self.updateAsset(assetName, assetUrl)
+    for _, asset in ipairs(assets) do
+      if asset.name == assetName then
+        if asset.url ~= assetUrl then
+          asset.url = assetUrl
+          assetsChanged = true
+        end
+        return
+      end
+    end
+
+    table.insert(assets, { name = assetName, url = assetUrl, })
+    assetsChanged = true
+  end
+
+  ---@param assetName string
+  ---@return boolean
+  function self.removeAsset(assetName)
+    for i, asset in ipairs(assets) do
+      if asset.name == assetName then
+        table.remove(assets, i)
+        return true
+      end
+    end
+
+    return false
+  end
+
+  ---@return tts__UIAsset[]
+  function self.getAssets()
+    return assets
+  end
+
+  ---@param assetName string
+  ---@return boolean
+  function self.hasAsset(assetName)
+    for _, asset in ipairs(assets) do
+      if asset.name == assetName then
+        return true
+      end
+    end
+    return false
+  end
+
+  return self
+end
+
+setmetatable(XmlUi, TableUtil.merge(getmetatable(XmlUiContainer), {
+  ---@param object tts__Object
+  __call = function(_, object)
+    return new(object)
+  end
 }))
 
 ---@param object tts__Object
 ---@param assetName string
 ---@return boolean
 function XmlUi.hasAsset(object, assetName)
-    local assets = object.UI.getCustomAssets()
+  local assets = object.UI.getCustomAssets()
 
-    for _, asset in ipairs(assets) do
-        if asset.name == assetName then
-            return true
-        end
+  for _, asset in ipairs(assets) do
+    if asset.name == assetName then
+      return true
     end
+  end
 
-    return false
+  return false
 end
 
 return XmlUi
