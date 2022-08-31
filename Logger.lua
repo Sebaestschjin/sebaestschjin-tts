8d135643658bc20f36329867ab9ffa9f9f21cda6
Sebastian Stern <sebastian.stern.42@gmail.com>
2022-08-31 22:33:20 +0200

    fix: Only call register object logger on Global, if it really exists

--- Logger.lua
+++ Logger.lua
@@ -15,18 +15,18 @@ Logger.VERBOSE = GeLogger.VERBOSE
 
 ---@type table<number, string>
 local levelPrefixes = {
-    [GeLogger.ERROR] = 'ERROR: ',
-    [GeLogger.WARNING] = 'WARNING: ',
-    [GeLogger.INFO] = 'INFO: ',
-    [GeLogger.DEBUG] = 'DEBUG: ',
-    [GeLogger.VERBOSE] = 'VERBOSE: ',
+  [GeLogger.ERROR] = 'ERROR: ',
+  [GeLogger.WARNING] = 'WARNING: ',
+  [GeLogger.INFO] = 'INFO: ',
+  [GeLogger.DEBUG] = 'DEBUG: ',
+  [GeLogger.VERBOSE] = 'VERBOSE: ',
 }
 
 ---@type table<ge_tts__Logger_LogLevel, string>
 local levelColors = {
-    [GeLogger.ERROR] = 'Red',
-    [GeLogger.WARNING] = 'Yellow',
-    [GeLogger.INFO] = 'Blue',
+  [GeLogger.ERROR] = 'Red',
+  [GeLogger.WARNING] = 'Yellow',
+  [GeLogger.INFO] = 'Blue',
 }
 
 --- Logger instances registered on other objects.
@@ -34,97 +34,100 @@ local levelColors = {
 local objectLoggers = {}
 
 setmetatable(Logger, TableUtils.merge(getmetatable(GeLogger), {
-    __call = function()
-        local self = GeLogger()
+  __call = function()
+    local self = GeLogger()
 
-        ---@param message string
+    ---@param message string
         ---@param level ge_tts__Logger_LogLevel
-        function self.log(message, level)
-            printToAll(levelPrefixes[level] .. message, levelColors[level])
-        end
+    function self.log(message, level)
+      printToAll(levelPrefixes[level] .. message, levelColors[level])
+    end
 
-        return self
-    end,
-    __index = GeLogger,
+    return self
+  end,
+  __index = GeLogger,
 }))
 
 local logger = Logger()
 
 local function buildMessage(...)
-    local args = table.pack(...)
-    for i = 1, args.n do
-        args[i] = logString(args[i])
-    end
-    return string.format(table.unpack(args))
+  local args = table.pack(...)
+  for i = 1, args.n do
+    args[i] = logString(args[i])
+  end
+  return string.format(table.unpack(args))
 end
 
 ---@param functionName string
 local function notifyObjectLoggers(functionName, parameter)
-    if self == Global then
-        for i = #objectLoggers, 1, -1 do
-            local objectLogger = objectLoggers[i]
-            local obj = getObjectFromGUID(objectLogger)
-            if obj then
-                (--[[---@not nil]] obj).call(functionName, parameter)
-            else
-                table.remove(objectLoggers, i)
-            end
-        end
+  if self == Global then
+    for i = #objectLoggers, 1, -1 do
+      local objectLogger = objectLoggers[i]
+      local obj = getObjectFromGUID(objectLogger)
+      if obj then
+        (--[[---@not nil]] obj).call(functionName, parameter)
+      else
+        table.remove(objectLoggers, i)
+      end
     end
+  end
 end
 
 ---@param level ge_tts__Logger_LogLevel
 function Logger.setLevel(level)
-    logger.setFilterLevel(level)
-    notifyObjectLoggers("__set_logger_level", level)
+  logger.setFilterLevel(level)
+  notifyObjectLoggers("__set_logger_level", level)
 end
 
 function Logger.error(...)
-    if logger.getFilterLevel() >= GeLogger.ERROR then
-        logger.log(buildMessage(...), GeLogger.ERROR)
-    end
+  if logger.getFilterLevel() >= GeLogger.ERROR then
+    logger.log(buildMessage(...), GeLogger.ERROR)
+  end
 end
 
 function Logger.warn(...)
-    if logger.getFilterLevel() >= GeLogger.WARNING then
-        logger.log(buildMessage(...), GeLogger.WARNING)
-    end
+  if logger.getFilterLevel() >= GeLogger.WARNING then
+    logger.log(buildMessage(...), GeLogger.WARNING)
+  end
 end
 
 function Logger.info(...)
-    if logger.getFilterLevel() >= GeLogger.INFO then
-        logger.log(buildMessage(...), GeLogger.INFO)
-    end
+  if logger.getFilterLevel() >= GeLogger.INFO then
+    logger.log(buildMessage(...), GeLogger.INFO)
+  end
 end
 
 function Logger.debug(...)
-    if logger.getFilterLevel() >= GeLogger.DEBUG then
-        logger.log(buildMessage(...), GeLogger.DEBUG)
-    end
+  if logger.getFilterLevel() >= GeLogger.DEBUG then
+    logger.log(buildMessage(...), GeLogger.DEBUG)
+  end
 end
 
 function Logger.verbose(...)
-    if logger.getFilterLevel() >= GeLogger.VERBOSE then
-        logger.log(buildMessage(...), GeLogger.VERBOSE)
-    end
+  if logger.getFilterLevel() >= GeLogger.VERBOSE then
+    logger.log(buildMessage(...), GeLogger.VERBOSE)
+  end
 end
 
 function Logger.assert(value, ...)
-    if not value then
-        logger.assert(value, buildMessage(...))
-    end
+  if not value then
+    logger.assert(value, buildMessage(...))
+  end
 end
 
 if self == Global then
-    ---@param guid GUID
-    _G.__register_object_logger = function(guid)
-        table.insert(objectLoggers, guid)
-    end
+  ---@param guid GUID
+  _G.__register_object_logger = function(guid)
+    table.insert(objectLoggers, guid)
+  end
+  _G.__logger_exists = true
 else
+  if Global.getVar("__logger_exists") then
     Global.call("__register_object_logger", self.getGUID())
-    _G.__set_logger_level = function(level)
-        Logger.setLevel(level)
-    end
+  end
+  _G.__set_logger_level = function(level)
+    Logger.setLevel(level)
+  end
 end
 
 return Logger
